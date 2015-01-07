6597cab407364b467fc185873a078fca1fcb8818
Ulf Dunkel <dunkel@dsd.net>
2015-01-07 11:33:11 +0100

    deprecated Gestalt(gestaltSystemVersion, &MacVersion) replaced

--- Source/Utils.m
+++ Source/Utils.m
@@ -7,6 +7,7 @@
 //
 
 #import "Utils.h"
+#include <Availability.h>
 
 
 @implementation Utils
@@ -38,6 +39,53 @@ static NSThread *mainThread = nil;
 
 + (SInt32)getOSVersion
 {
+    static SInt32 exactVersion = 0x00;
+    
+    if (0x00 == exactVersion)
+    {
+        @synchronized(self)
+        {
+            if (0x00 == exactVersion)
+            {
+                if ([NSProcessInfo instancesRespondToSelector:@selector(operatingSystemVersion)])
+                {
+                    NSOperatingSystemVersion sysVersion = [[NSProcessInfo processInfo] operatingSystemVersion];
+
+                    switch (sysVersion.majorVersion)
+                    {
+                        case 11:
+                            exactVersion = 0x01100;
+                            break;
+                            
+                        case 10:
+                            exactVersion = 0x01000;
+                            break;
+                            
+                        case  9:
+                            exactVersion = 0x0900;
+                            break;
+                            
+                        case  8:
+                            exactVersion = 0x0800;
+                            break;
+                            
+                        case  7:
+                            exactVersion = 0x0700;
+                    }
+                    
+                    exactVersion = exactVersion | ((sysVersion.minorVersion & 0x0F) << 4) | (sysVersion.patchVersion & 0x0F);
+                }
+            }
+        }
+    }
+    
+    return exactVersion;
+}
+
+
+/* fd:2015-01-07 replaced by new version (see above) after a hint from Thorsten Lemke
++ (SInt32)getOSVersion
+{
 	SInt32 MacVersion;
 	if (Gestalt(gestaltSystemVersion, &MacVersion) == noErr) {
 		return MacVersion;
@@ -45,6 +93,7 @@ static NSThread *mainThread = nil;
 		return 0;
 	}
 }
+*/
 
 + (BOOL)isOSVersionTigerAndBelow:(SInt32)version
 {
